6f91dc533dc879bfcaac98a4974827cbb150244f
hasanKakeh <hasan.kakeh1@gmail.com>
2023-10-08 00:21:24 +0300

    provide assets interceptor in app server

--- src/app/root/app.server.module.ts
+++ src/app/root/app.server.module.ts
@@ -3,12 +3,14 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { provideAssetsInterceptor } from '@core/interceptor';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
   ],
+  providers: [provideAssetsInterceptor()],
   bootstrap: [AppComponent],
 })
-export class AppServerModule {}
+export class AppServerModule { }
